7217290402b5650f2399b7cab1e7ff1ca4b9985c
Indhu-AR <155819612+Indhu-AR@users.noreply.github.com>
2026-06-13 16:12:26 +0530

    Add files via upload

--- Challenge1.java
+++ Challenge1.java
@@ -1,14 +1,18 @@
-package Task2;
+package Task3;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Challenge1 {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Indhu\\OneDrive\\Desktop\\Java\\SeleniumLearning\\Driver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.facebook.com");
+		  public static void main(String[] args) throws Exception{
+	            System.setProperty("webdriver.chrome.driver","C:\\Users\\Indhu\\OneDrive\\Desktop\\Java\\SeleniumLearning\\Driver\\chromedriver.exe");
+	            WebDriver driver=new ChromeDriver();
+	            driver.get("https://www.facebook.com/");
+	            Thread.sleep(5000);
+	            WebElement email=driver.findElement(By.name("email"));
+	            System.out.println("Height: " + email.getSize().getHeight());
+	            System.out.println("Width: " + email.getSize().getWidth());
+	            System.out.println("X location: " + email.getLocation().getX());
+	            System.out.println("Y location: " + email.getLocation().getY());
 
-        driver.findElement(By.name("email")).sendKeys("test123");
-        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("password");
-    }
+	        } 
 }
