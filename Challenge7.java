0b874fb01b85fe239a17ad8b441a701d59ba469a
Indhu-AR <155819612+Indhu-AR@users.noreply.github.com>
2026-06-13 16:10:43 +0530

    Add files via upload

---
+++ Challenge7.java
@@ -0,0 +1,16 @@
+package Task2;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Challenge7 {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Indhu\\OneDrive\\Desktop\\Java\\SeleniumLearning\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.flipkart.com");
+        Thread.sleep(3000);
+        driver.findElement(By.name("q")).sendKeys("Redmi phone");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+    }
+}
+
