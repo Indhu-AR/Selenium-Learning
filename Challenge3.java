c2cbb997f7c44aebd9d19f158734e4fad2f69e45
Indhu-AR <155819612+Indhu-AR@users.noreply.github.com>
2026-06-13 16:14:47 +0530

    Add files via upload

--- Challenge3.java
+++ Challenge3.java
@@ -1,21 +1,24 @@
-package Task2;
-
-import org.openqa.selenium.*;
+package Task3;
+import java.io.File;
+import java.io.IOException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.io.FileHandler;
 public class Challenge3 {
-    public static void main(String[] args) throws InterruptedException {
-
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Indhu\\OneDrive\\Desktop\\Java\\SeleniumLearning\\Driver\\chromedriver.exe");
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+	public static void main(String[] args) throws InterruptedException, IOException {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Indhu\\OneDrive\\Desktop\\Java\\SeleniumLearning\\Driver\\chromedriver.exe");        
+        WebDriver driver =new ChromeDriver();
         driver.get("https://www.amazon.in");
-        Thread.sleep(3000);  
+        Thread.sleep(5000);
+        System.out.println("Page Title:"+driver.getTitle());        
+        String frame = "error003";
+        TakesScreenshot ts=(TakesScreenshot)driver;
+        File temp=ts.getScreenshotAs(OutputType.FILE);
+        File src = new File("./ErrorShots/" + frame + ".png");
+        FileHandler.copy(temp,src);
 
-        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
-        searchBox.sendKeys("iphone");
-        driver.findElement(By.id("nav-search-submit-button")).click();
-        Thread.sleep(5000);  
-    }
+	}
+
 }
