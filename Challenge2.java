5d74672fa605aabef953584d4e2f00964920ac43
Indhu-AR <155819612+Indhu-AR@users.noreply.github.com>
2026-06-13 16:12:43 +0530

    Add files via upload

--- Challenge2.java
+++ Challenge2.java
@@ -1,20 +1,21 @@
-package Task2;
-import org.openqa.selenium.*;
+package Task3;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Challenge2 {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Indhu\\OneDrive\\Desktop\\Java\\SeleniumLearning\\Driver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.youtube.com");
-
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://www.facebook.com/r.php");
         Thread.sleep(3000);
-        driver.findElement(By.name("search_query")).sendKeys("India");
-    	WebElement searchBtn = driver.findElement(By.id("search-icon-legacy"));
-        JavascriptExecutor js=(JavascriptExecutor)driver;
-		js.executeScript("arguments[0].click(0)", searchBtn);
+        List<WebElement> dropdowns=driver.findElements(By.xpath("//div[@role='combobox']"));
+        WebElement gender=dropdowns.get(3);
+        WebElement male=driver.findElement(By.xpath("//span[text()='Male']"));
+        male.click();
+        System.out.println("Male is selected!!!");
+        driver.quit();
+	}
 
- 
-    }
 }
-
